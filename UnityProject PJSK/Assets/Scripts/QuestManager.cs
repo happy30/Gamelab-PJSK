dbc713fba549f2db93c4000d827b90b90d4598d3
happy30 <jordivdh@hotmail.com>
2016-05-19 23:48:42 +0200

    added two functions to questmanager for activating and completing quests

--- UnityProject PJSK/Assets/Scripts/QuestManager.cs
+++ UnityProject PJSK/Assets/Scripts/QuestManager.cs
@@ -4,4 +4,23 @@ using System.Collections;
 public class QuestManager : MonoBehaviour
 {
     public QuestClass[] quests;
+    public InventoryManager inventory;
+
+    void Start()
+    {
+        inventory = GetComponent<InventoryManager>();
+    }
+
+    public void ActivateQuest(int questID)
+    {
+        quests[questID].questState = QuestClass.QuestState.Active;
+    }
+
+    public void CompleteQuest(int questID)
+    {
+        quests[questID].questState = QuestClass.QuestState.Completed;
+        inventory.changePiggies(quests[questID].piggieReward);
+    }
 }
+
+
