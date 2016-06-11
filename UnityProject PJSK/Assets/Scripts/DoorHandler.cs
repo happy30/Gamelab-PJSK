e912675f4dc804f24ca211f6ea1444d56c6f3966
Jordi van der Hulst <Jordi van der Hulst>
2016-06-11 17:07:49 +0200

    Completed hubhouse and playerhouse. added script for door handling

---
+++ UnityProject PJSK/Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorHandler : MonoBehaviour {
+
+    public InteractScript interact;
+    Animator _anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        interact = GetComponent<InteractScript>();
+        _anim = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(interact.interacted)
+        {
+            if (!_anim.GetBool("OpenDoor"))
+            {
+                _anim.SetBool("OpenDoor", true);
+            }
+            else
+            {
+                _anim.SetBool("OpenDoor", false);
+            }
+            interact.interacted = false;
+        }
+	}
+}
