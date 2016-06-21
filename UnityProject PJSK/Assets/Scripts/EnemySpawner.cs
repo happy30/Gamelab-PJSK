397cb3f3439d625408603a1e8523ca5bbcbef04c
sascha greve <sascha greve>
2016-06-21 14:34:56 +0200

    Added: Enemy AI

--- UnityProject PJSK/Assets/Scripts/EnemySpawner.cs
+++ UnityProject PJSK/Assets/Scripts/EnemySpawner.cs
@@ -7,29 +7,33 @@ public class EnemySpawner : MonoBehaviour {
     public GameObject spawnedEnemy;
     public float timer;
     public float spawnTimeAfterDeath;
+    public int enemyCount;
+    public int enemyLimit;
     public float minSpawnTime;
     public float maxSpawnTime;
 
     // Use this for initialization
     void Start ()
     {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         timer += Time.deltaTime;
-        if (timer >= spawnTimeAfterDeath)
+        if (timer >= spawnTimeAfterDeath && enemyCount < enemyLimit)
         {
             SpawnEnemy ();
             timer = 0;
+            enemyCount ++;
         }
 	}
 
     public void SpawnEnemy ()
     {
         spawnedEnemy = (GameObject)Instantiate(enemy, transform.position, Quaternion.identity);
+        spawnedEnemy.GetComponent<EnemyBehaviour>().enemySpawner = gameObject;
         spawnTimeAfterDeath = (Random.Range(minSpawnTime, maxSpawnTime));
     }
 }
